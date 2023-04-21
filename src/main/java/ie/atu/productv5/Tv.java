6c4ad788c345286017e304a72ec1252eca3a8a3e
G00402357@atu.ie <G00402357@atu.ie>
2023-04-21 12:31:50 +0100

    added tv information

---
+++ src/main/java/ie/atu/productv5/Tv.java
@@ -0,0 +1,58 @@
+package ie.atu.productv5;
+
+public class Tv {
+    private double price;
+    private String description;
+    private String code;
+    private String screenSize;
+    private String manufacture;
+
+    public Tv(){
+        price = "0.0";
+        description = "";
+        code = "";
+        screenSize = "";
+        manufacture = "";
+
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getScreenSize() {
+        return screenSize;
+    }
+
+    public void setScreenSize(String screenSize) {
+        this.screenSize = screenSize;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+}
