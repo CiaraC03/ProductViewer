f9800b3198f5ed59f900e59f841ac2dc52a5274c
G00402357@atu.ie <G00402357@atu.ie>
2023-04-21 12:27:38 +0100

    added music information

--- src/main/java/ie/atu/productv5/Music.java
+++ src/main/java/ie/atu/productv5/Music.java
@@ -1,5 +1,58 @@
 package ie.atu.productv5;
 
 public class Music {
+    private double price;
+    private String description;
+    private String code;
+    private String artist;
+    private String label;
+
+    public Music(){ //empty string
+        price = "0.0";
+        description = "";
+        code = "";
+        artist = "";
+        label = "";
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
 
 }
